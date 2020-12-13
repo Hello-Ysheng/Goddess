e987d7f853472639f2f65ed0a66cb2b94c7e333c
Hello-Ysheng <ysh1351221@163.com>
2020-12-13 21:24:48 +0800

    first commit

---
+++ chinesegoddess-entity/src/main/java/Person.java
@@ -0,0 +1,38 @@
+public class Person {
+    private String name;
+    private String cardID;
+    private Integer age;
+    private String company;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCardID() {
+        return cardID;
+    }
+
+    public void setCardID(String cardID) {
+        this.cardID = cardID;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+}
---
+++ chinesegoddess-service/src/main/java/impl/CGoddessServiceImpl.java
@@ -0,0 +1,7 @@
+/**
+ * @Author:sye
+ * @Date:2020-12-12 10:56
+ * @Ant:
+ */
+public class CGoddessServiceImpl implements ICGoddessService {
+}
